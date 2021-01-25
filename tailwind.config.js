cead582fcc178bc351ba45ed66f871f3ee7be3c3
Thomas Le Naour <lenaourthomas@gmail.com>
2021-01-25 17:21:30 +0100

    :bug: enable purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: {
-    enabled: false,
+    enabled: true,
     content: ['./src/**/*.{js,jsx,ts,tsx}', './src/**/**/*.{js,jsx,ts,tsx}', './src/**/**/**/*.{js,jsx,ts,tsx}'],
     options: {
       safelist: ['bg-gray-300', 'bg-red-300', 'bg-yellow-300', 'bg-green-300', 'bg-blue-300', 'bg-indigo-300', 'bg-purple-300', 'bg-pink-300']
